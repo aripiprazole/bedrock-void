ba84e60d6631c0eb9f7f12cb60a07ef5bcfcbe4b
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-10-14 05:24:53 -0300

    fix: fix packet id

--- src/jvmMain/kotlin/protocol/packets/inbound/ClientCacheStatusPacket.kt
+++ src/jvmMain/kotlin/protocol/packets/inbound/ClientCacheStatusPacket.kt
@@ -3,5 +3,5 @@ package com.gabrielleeg1.bedrockvoid.protocol.packets.inbound
 import com.gabrielleeg1.bedrockvoid.protocol.packets.InboundPacket
 import com.gabrielleeg1.bedrockvoid.protocol.packets.Packet
 
-@Packet(0x01)
+@Packet(0x81)
 data class ClientCacheStatusPacket(val supported: Boolean) : InboundPacket
